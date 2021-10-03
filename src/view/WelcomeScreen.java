2734a51595ac6e1e65128468b2fea45ebc2ec002
pnguyen99 <pnguyen99@gatech.edu>
2021-10-03 15:06:20 -0400

    Refector and Update Funtionality

--- src/view/WelcomeScreen.java
+++ src/view/WelcomeScreen.java
@@ -1,8 +1,7 @@
 package view;
 
-import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -12,39 +11,37 @@ import javafx.scene.layout.VBox;
 public class WelcomeScreen {
     private int width;
     private int height;
-    //changed type to imageView
-    Image strtBtn1 = new Image("images//startButton1.png");
-    Image strtBtn2 = new Image("images//startButton2.png");
+    private Image startButtonDefault = new Image("images//startButton1.png");
+    private Image startButtonHovered = new Image("images//startButton2.png");
     private ImageView startButton;
 
     public WelcomeScreen(int width, int height) {
         this.width = width;
         this.height = height;
-        //startButton = new Button("Start Game");
-
-        //Start Button 1
-        startButton = new ImageView(strtBtn1);
+        this.startButton = new ImageView(startButtonDefault);
     }
 
     public Scene getScene() {
         Label label = new Label("Welcome to Tower Defense!");
         HBox button = new HBox(startButton);
-        button.setPadding(new Insets(200, 5, 5, 20));
-        //VBox vbox = new VBox(label, button);
+        button.setAlignment(Pos.TOP_CENTER);
         VBox vbox = new VBox(label, button);
-        vbox.setPadding(new Insets(15, 5, 5, 170));
+        vbox.setAlignment(Pos.TOP_CENTER);
+        vbox.setSpacing(200);
         Scene scene = new Scene(vbox, width, height);
         return scene;
     }
-    //switched return type from Button to ImageView
+
+
     public ImageView getStartButton() {
         return startButton;
     }
+
     public void toggleStartButton() {
-        if (startButton.getImage().equals(strtBtn1)) {
-            startButton.setImage(strtBtn2);
+        if (startButton.getImage().equals(startButtonDefault)) {
+            startButton.setImage(startButtonHovered);
         } else {
-            startButton.setImage(strtBtn1);
+            startButton.setImage(startButtonDefault);
         }
     }
 }
