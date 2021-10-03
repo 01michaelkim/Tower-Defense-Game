4768fda690f302e9fb9d28c036b1e909ee5922fc
Minhyuk Kim <mkim792@gatech.edu>
2021-10-03 13:48:00 -0400

    Updates to the game screen

--- src/view/GameScreen.java
+++ src/view/GameScreen.java
@@ -13,6 +13,8 @@ public class GameScreen {
     private int width;
     private int height;
     private Button startButton;
+    private int health = 100;
+    private int money = 500;
 
     public GameScreen(int width, int height) {
         this.width = width;
@@ -24,7 +26,10 @@ public class GameScreen {
         Label label = new Label("Tower Defense");
         Image back = new Image("images//map.png");
         ImageView background = new ImageView(back);
-        root.getChildren().addAll(label, background);
+        Label healthLabel = new Label("Health: " + health);
+        Label moneyLabel = new Label("Money: " + money);
+        VBox vbox = new VBox(healthLabel, moneyLabel);
+        root.getChildren().addAll(label, background, vbox);
         Scene scene = new Scene(root, width, height);
         return scene;
     }
